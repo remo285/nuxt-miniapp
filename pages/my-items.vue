80e693dd5d8486d4cf2359a653386ad3e52ef2e2
remo285 <fran9910@gmail.com>
2021-04-14 02:32:01 -0300

    first commit

---
+++ pages/my-items.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <MyItem
+            v-for="item in myRentals"
+            :key="item.id"
+            :item="item"
+        />
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex"
+    export default {
+        layout: "no-nav",
+        computed: {
+            ...mapState(['myRentals'])
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
